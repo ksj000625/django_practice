64a86fbcbc5ec2c1f17d0be6b30f058033eb8ef4
Kim Seongjin <darkmoon625@naver.com>
2023-07-13 15:22:46 +0900

    Change list customizing

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 
 # class for question and published_date
@@ -10,6 +11,11 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def __str__(self):
         return self.question_text
 
